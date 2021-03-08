788412f7e9e47960dc012a4f8e0a524914d11848
Makoto Inoue <2630+makoto@users.noreply.github.com>
2021-03-08 19:19:03 +0000

    Remove console.log

--- test/test_oracle.ts
+++ test/test_oracle.ts
@@ -107,7 +107,6 @@ describe('dnsoraclejs', async () => {
     });
 
     it('processes a real record on TXT _ens.matoken.live correctly', async () => {
-        console.log('ethers.provider', ethers.provider)
         const oracle = new Oracle(oracleContract.address, ethers.provider);
 
         const prover = makeProver(MATOKEN_1);
